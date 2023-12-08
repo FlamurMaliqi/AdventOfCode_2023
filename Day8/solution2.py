99687bf82b93d3448a74edd728edb752fe18c72a
FlamurMaliqi <120040061+FlamurMaliqi@users.noreply.github.com>
2023-12-08 12:06:04 +0100

    Lebron

--- Day8/solution2.py
+++ Day8/solution2.py
@@ -1,74 +1,36 @@
-file = open("input.txt", "r")
+from typing import Callable
+import math
 
+class Node:
+    next: tuple["Node"]
 
-sequence  = "LLRRRLLRLRRRLLRLRLRLRLRRRLRRLRRLRLLLRRLLRRLRRLRRLRRRLLLRRLRLRRRLRRRLRLRRLRRRLRLRRRLRLRLLLRLRRLRLRRLRRRLRLRRRLRRRLRRRLRRRLRLRRRLRRRLRLLRRLRLRLRRRLRRLRRRLRRRLRRRLRRRLLLLRRLLRLRRLRRLRRRLRRRLLLRRLRRLRLRRLRRRLRRLRLRRRLRLRRLLRLLRRLRLRRRLRRLRRLRLRRLLLRRRLRLRRRLRLRLLRLRLRRRLRLRLRRRLRRLRRLRRRLRRLLRRRR"
-# for zeile in file:
-#     counter += 1
-#     if counter==2:
-#         break
-#     sequence=sequence+zeile
-# print(sequence+"end")
-# sequence.replace("\n","").replace(" ","")
-# print(sequence + "end")
+with open("input.txt") as src:
+    route, __, *nodes = map(str.strip, src)
+    [*route] = map(int, route.translate(str.maketrans("LR", "01")))
 
+for n in nodes:
+    exec(n[:6]+"Node()")
 
-bitch = {}
+for n in nodes:
+    exec(n.replace(" =", ".next ="))
 
-counter = 0
+def n_routes(
+    pos: Node=AAA,
+    condition: Callable=lambda n: n==ZZZ,
+) -> int:
+    count = 1
+    while True:
+        for d in route:
+            pos = pos.next[d]
+        if condition(pos):
+            break
+        count += 1
+    return count
 
-for zeile in file:
-        if counter>1:
-            temp = zeile.split(" = ")
-            tupel = temp[1].split(",")
-            left = tupel[0].replace("(","")
-            right = tupel[1].replace(")","").replace("\n","").replace(" ","")
-            bitch[temp[0]] = (left,right)
-        counter += 1
+print("part one answer:", n_routes()*len(route))
 
+starts = [v for k, v in locals().items() if k.endswith("A")]
+ends = [v for k, v in locals().items() if k.endswith("Z")]
+counts = [n_routes(n, lambda n: n in ends) for n in starts]
 
-
-
-# def rekursiv(value, char, counter):
-#     if(value == 'ZZZ'):
-#         return counter
-#     counter += 1
-#     if(char == 'L'):
-#         rekursiv(bitch[value][0],sequence[counter%len(sequence)], counter)
-#     else:
-#         rekursiv(bitch[value][1],sequence[counter%len(sequence)], counter)
-# print(rekursiv('AAA','L',0))
-    
-
-counter3=0
-counter_durchlauf = 0
-def check_end(value):
-    while(value.endswith('Z')):
-        for char in sequence:
-            # if counter3== 0:
-            #      break
-            if value.endswith('Z'):
-                return true 
-            if value == "":
-                value = 'AAA'
-            if char == 'L':
-                value = bitch[value][0]
-            else:
-                value = bitch[value][1]
-            counter_durchlauf += 1
-        # counter3 = 1
-
-while(True):
-    if (check_end('GPA') and check_end('GTA') and check_end('VDA') and check_end('BBA') and check_end('AAA') and check_end('VSA')):
-        print(counter_durchlauf)
-
-
-print(counter_durchlauf)
-list = []
-for key in bitch:
-    if key.endswith('A'):
-        list.append(key)
-print(list)
-
-      
-
-
+print("part two answer:", math.lcm(*counts)*len(route))
